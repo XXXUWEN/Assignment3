4132b9ea2bdc3680059dbd4112bac00694e120ad
xuwen nie <niexuwen@tad60.wpa.dal.ca>
2015-10-08 14:39:48 -0300

    Initial Commit

---
+++ Assignment3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment3
+//
+//  Created by xuwen nie on 15/10/8.
+//  Copyright © 2015年 xuwen nie. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
